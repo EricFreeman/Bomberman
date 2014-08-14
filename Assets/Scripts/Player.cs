27ced0693101c6b9c78354872aec86b3e5c971af
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-13 18:28:17 -0600

    Added a bunch of temp images and basic movement for player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool IsDead = false;
+    public float MoveSpeed = 1.5f;
+
+    void Update()
+    {
+        if (!IsDead)
+        {
+            transform.Translate(
+                Input.GetAxisRaw("Horizontal") * Time.deltaTime * MoveSpeed,
+                Input.GetAxisRaw("Vertical") * Time.deltaTime * MoveSpeed,
+                0);
+        }
+    }
+}
