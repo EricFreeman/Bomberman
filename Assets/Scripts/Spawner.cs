956c1aebcbc3b46aaf48e613746be580e5cce549
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-20 08:53:53 -0600

    Updated spawner to take in an x and y offset that will adjust all waypoints accordingly.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -6,7 +7,9 @@ namespace Assets.Scripts
     public class Spawner : MonoBehaviour
     {
         public List<Transform> ToSpawn;
-        public List<Vector3> Waypoints; 
+        public List<Vector3> Waypoints;
+        public float XVariation;
+        public float YVariation;
 
         public int MinSpawnDelay;
         public int MaxSpawnDelay;
@@ -27,9 +30,23 @@ namespace Assets.Scripts
         {
             var obj = ToSpawn[Random.Range(0, ToSpawn.Count)];
 
-            var newObj = (GameObject)Instantiate(obj.gameObject, transform.position, Quaternion.Euler(0, 0, SpawnRotation));
-            newObj.AddComponent<WaypointMovement>().Waypoints = Waypoints;
+            var offset = GetSpawnOffset();
+            var newObj = (GameObject)Instantiate(obj.gameObject, 
+                transform.position + offset, 
+                Quaternion.Euler(0, 0, SpawnRotation));
+
+            newObj.AddComponent<WaypointMovement>().Waypoints = CreateNewWaypointList(offset);
             newObj.GetComponent<WaypointMovement>().MoveSpeed = 1;
+        }
+
+        private Vector3 GetSpawnOffset()
+        {
+            return new Vector3(Random.Range(-XVariation, XVariation), Random.Range(-YVariation, YVariation));
+        }
+
+        private List<Vector3> CreateNewWaypointList(Vector3 offset)
+        {
+            return Waypoints.Select(point => point + offset).ToList();
         }
     }
 }
