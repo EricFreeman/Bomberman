9778367df1ddbd161bfb3e8cdf042d4c33ba7f97
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-19 18:27:19 -0600

    Added a movement for objects based on waypoints to simulate people walking down a sidewalk in the example level.  Can be used for any object (but why would bus chairs or barrels be walking down a sidewalk).

---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WaypointMovement : MonoBehaviour
+    {
+        public float MoveSpeed;
+        public List<Vector3> Waypoints;
+
+        private Vector3 _currentWaypoint;
+        private int _index = 0;
+
+        void Start()
+        {
+            _currentWaypoint = Waypoints.Count > 0 ? Waypoints[0] : Vector3.zero;
+        }
+
+        void Update()
+        {
+            if (Waypoints == null || Waypoints.Count == 0) return;
+
+            transform.position = Vector3.MoveTowards(transform.position, _currentWaypoint, MoveSpeed*Time.deltaTime);
+            if (transform.position == _currentWaypoint)
+            {
+                if (_index++ == Waypoints.Count - 1) _index = 0;
+                _currentWaypoint = Waypoints[_index];
+            }
+        }
+    }
+}
