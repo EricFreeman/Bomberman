a48f0be4f58ed26777c036c050091e1b169e2237
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-20 08:43:49 -0600

    Added npc spawners.  They are very simple for now.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spawner : MonoBehaviour
+    {
+        public List<Transform> ToSpawn;
+        public List<Vector3> Waypoints; 
+
+        public int MinSpawnDelay;
+        public int MaxSpawnDelay;
+
+        public float SpawnRotation;
+
+        private int _spawnDelay;
+
+        void Update()
+        {
+            if (ToSpawn.Count == 0 || _spawnDelay-- > 0) return;
+
+            SpawnObject();
+            _spawnDelay = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+        }
+
+        private void SpawnObject()
+        {
+            var obj = ToSpawn[Random.Range(0, ToSpawn.Count)];
+
+            var newObj = (GameObject)Instantiate(obj.gameObject, transform.position, Quaternion.Euler(0, 0, SpawnRotation));
+            newObj.AddComponent<WaypointMovement>().Waypoints = Waypoints;
+            newObj.GetComponent<WaypointMovement>().MoveSpeed = 1;
+        }
+    }
+}
