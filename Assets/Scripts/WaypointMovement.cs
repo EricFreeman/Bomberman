619eecc59a31176d392009e6061428d92866c599
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-20 08:22:10 -0600

    Turned off waypoint movement when the breakable object is broken.

--- Assets/Scripts/WaypointMovement.cs
+++ Assets/Scripts/WaypointMovement.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts
         public List<Vector3> Waypoints;
 
         private Vector3 _currentWaypoint;
-        private int _index = 0;
+        private int _index;
 
         void Start()
         {
@@ -19,13 +19,33 @@ namespace Assets.Scripts
         void Update()
         {
             if (Waypoints == null || Waypoints.Count == 0) return;
+            if (IsBroken()) return;
 
+            MoveToWaypoint();
+            TurnTowardsWaypoint();
+        }
+
+        private void MoveToWaypoint()
+        {
             transform.position = Vector3.MoveTowards(transform.position, _currentWaypoint, MoveSpeed*Time.deltaTime);
             if (transform.position == _currentWaypoint)
             {
                 if (_index++ == Waypoints.Count - 1) _index = 0;
                 _currentWaypoint = Waypoints[_index];
             }
+        }
+
+        private void TurnTowardsWaypoint()
+        {
+            var lookDir = _currentWaypoint - transform.position;
+            transform.rotation = Quaternion.Euler(
+                new Vector3(0, 0, Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg - 90));
+        }
+
+        private bool IsBroken()
+        {
+            var bo = GetComponent<BreakableObject>();
+            return bo != null && bo.IsBroken;
         }
     }
 }
