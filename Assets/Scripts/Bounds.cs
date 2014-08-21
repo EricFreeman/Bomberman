dd02cdf4e4b3f7f90c7cf2e40f335bd8f2334221
Eric Freeman <eric.freeman@extendhealth.com>
2014-08-20 18:40:46 -0600

    Added a bounds script to either keep an object within bounds or remove an object if it goes out of bounds

---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Bounds : MonoBehaviour
+    {
+        public Vector3 MinBound;
+        public Vector3 MaxBound;
+
+        // Force position inside of bounds
+        public bool KeepInside = true;
+
+        // Delete object outside of bounds
+        public bool RemoveOutside;
+
+        private Rect _collisionBox;
+
+        void Start()
+        {
+            _collisionBox = new Rect
+            {
+                xMin = MinBound.x, 
+                yMin = MinBound.y, 
+                xMax = MaxBound.x, 
+                yMax = MaxBound.y
+            };
+        }
+
+        void Update()
+        {
+            if (_collisionBox.Contains(transform.position)) return;
+
+            if (KeepInside)
+                MoveInside();
+            else if(RemoveOutside)
+                Destroy(gameObject);
+        }
+
+        private void MoveInside()
+        {
+            if (transform.position.x < MinBound.x) transform.position = new Vector3(MinBound.x, transform.position.y);
+            if (transform.position.x > MaxBound.x) transform.position = new Vector3(MaxBound.x, transform.position.y);
+
+            if (transform.position.y < MinBound.y) transform.position = new Vector3(transform.position.x, MinBound.y);
+            if (transform.position.y > MaxBound.y) transform.position = new Vector3(transform.position.x, MaxBound.y);
+        }
+    }
+}
